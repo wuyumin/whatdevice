926b8a849f592bc17c47898389643eb4621a510c
吴育民 <wuyumin@wuyumin.com>
2016-08-31 12:04:45 +0800

    优化gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -17,10 +17,11 @@ var gulp = require('gulp'),
 
 // 定义一个scripts任务(可以自定义任务名称)
 gulp.task('scripts', function() {
+    console.log('压缩JS...');
     return gulp.src('whatdevice.js')
            .pipe(uglify())
            .pipe(header(banner, { pkg : pkg } ))
-           .pipe(rename('whatdevice.min.js'))
+           .pipe(rename({suffix:'.min'}))
            .pipe(gulp.dest('./'));
 });
 
