20eafb66d981346a7bb4632ac4d824d69b897499
吴育民 <wuyumin@wuyumin.com>
2016-11-23 14:21:19 +0800

    版本v3.0.0

--- whatdevice.js
+++ whatdevice.js
@@ -1,33 +1,31 @@
 /*!
  * 上网设备检测及处理
- * 版本：v2.2.3
+ * 版本：v3.0.0
  * 原创：吴育民
- * 网站：www.089858.com
+ * 网站：https://089858.com
  * 使用说明：https://github.com/wuyumin/whatdevice
  */
 ;(function(){
-    var whatdevice={};
-    var myUA=window.navigator.userAgent.toLowerCase();
-    var myReg=/(?:micromessenger|mobile|iphone|ipod|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|windows phone|win ce)/; //小写
+    var whatdevice = {};
+    var myUA = window.navigator.userAgent.toLowerCase();
+    //正则得小写
 
     /*
     * 是否移动设备：返回Boolean。
     */
-    whatdevice.isMobile=function(){
-        return myReg.test(myUA);
-    };
+    whatdevice.isMobile = /(?:micromessenger|mobile|iphone|ipod|android|coolpad|mmp|smartphone|midp|wap|xoom|symbian|j2me|blackberry|windows phone|win ce)/.test(myUA);
 
     /*
     * 跳转到手机网站：参数1是手机网站网址。
     * 使用方法：whatdevice.go2mob('手机网站网址') 若不想跳转，当前访问的URL传递参数itsme=1，如www.089858.com/?itsme=1
     */
-    whatdevice.go2mob=function(){
-        var mobUrl=arguments[0];//参数1
-        var myArg=[];
-        myArg=window.location.search.substr(1).match(/(^|&)itsme=([^&]*)(&|$)/i);//获取URL参数itsme
-        var itsme=(myArg!=null && myArg[2]!=null)?myArg[2]:0;
-        if(itsme!=1 && mobUrl!=null && whatdevice.isMobile()){
-            window.location.href=mobUrl;
+    whatdevice.go2mob = function(){
+        var mobUrl = arguments[0]; //参数1
+        var myArg = [];
+        myArg = window.location.search.substr(1).match(/(^|&)itsme=([^&]*)(&|$)/i); //获取URL参数itsme
+        var itsme = (myArg != null && myArg[2] != null)?myArg[2]:0;
+        if(itsme != 1 && mobUrl != null && whatdevice.isMobile){
+            window.location.href = mobUrl;
         }
     };
 
@@ -35,37 +33,31 @@
     * 跳转到电脑网站：参数1是电脑网站网址。
     * 使用方法：whatdevice.go2web('电脑网站网址') 若不想跳转，当前访问的URL传递参数itsme=1，如m.089858.com/?itsme=1
     */
-    whatdevice.go2web=function(){
-        var webUrl=arguments[0];//参数1
-        var myArg=[];
-        myArg=window.location.search.substr(1).match(/(^|&)itsme=([^&]*)(&|$)/i);//获取URL参数itsme
-        var itsme=(myArg!=null && myArg[2]!=null)?myArg[2]:0;
-        if(itsme!=1 && webUrl!=null && !whatdevice.isMobile()){
-            window.location.href=webUrl;
+    whatdevice.go2web = function(){
+        var webUrl = arguments[0]; //参数1
+        var myArg = [];
+        myArg = window.location.search.substr(1).match(/(^|&)itsme=([^&]*)(&|$)/i); //获取URL参数itsme
+        var itsme = (myArg != null && myArg[2] != null)?myArg[2]:0;
+        if(itsme != 1 && webUrl != null && !whatdevice.isMobile){
+            window.location.href = webUrl;
         }
     };
 
     /*
     * 是否苹果手机：返回Boolean。
     */
-    whatdevice.isiOS=function(){
-        return /(?:iphone)/.test(myUA);
-    };
+    whatdevice.isiOS = /(?:iphone)/.test(myUA);
 
     /*
     * 是否安卓手机：返回Boolean。
     */
-    whatdevice.isAndroid=function(){
-        return /(?:android)/.test(myUA);
-    };
+    whatdevice.isAndroid = /(?:android)/.test(myUA);
 
     /*
     * 是否微信浏览器：返回Boolean。
     */
-    whatdevice.isWechat=function(){
-        return /(?:micromessenger)/.test(myUA);
-    };
+    whatdevice.isWechat = /(?:micromessenger)/.test(myUA);
     
     /*注册全局变量*/
-    window.whatdevice=whatdevice;
+    window.whatdevice = whatdevice;
 })();
